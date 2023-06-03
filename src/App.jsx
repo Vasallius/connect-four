1f57f27613e08fd618dfc0c29f229fbc23d56aee
Jedidiah Carl O. Tan <masterjed7262@gmail.com>
2023-06-04 01:18:55 +0800

    feat: add MainMenu

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
 import "./App.css";
+import MainMenu from "./components/MainMenu";
 
 function App() {
-  return <div className="bg-">Connect Four</div>;
+  return (
+    <div className="bg-iris h-screen flex  justify-center">
+      <MainMenu />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/MainMenu.jsx
@@ -0,0 +1,20 @@
+import Logo from "../assets/images/logo.svg";
+import PlayervsPlayer from "../assets/images/player-vs-player.svg";
+
+const MainMenu = () => {
+  return (
+    <div className="bg-purple h-[435px] w-[480px] flex flex-col items-center mt-[120px] justify-center gap-y-2 rounded-[40px] border-[3px] border-black thick-shadow">
+      <img className="mt-[70px] mb-[79px]" src={Logo} alt="" />
+      <div className="flex items-center justify-between bg-dandelion rounded-[20px] p-5 w-[400px] border-[3px] border-black thick-shadow mb-[30px]">
+        <div className="text-md font-spacegrotesk font-bold ">
+          PLAY VS PLAYER
+        </div>
+        <img className="" src={PlayervsPlayer} alt="" />
+      </div>
+      <div className="flex justify-between bg-white rounded-[20px] p-5 w-[400px] border-[3px] border-black thick-shadow mb-[60px]">
+        <div className="text-md font-spacegrotesk font-bold ">GAME RULES</div>
+      </div>
+    </div>
+  );
+};
+export default MainMenu;
