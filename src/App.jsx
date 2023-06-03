a173fec60db60d1561ab376674ed4a4426469524
Jedidiah Carl O. Tan <masterjed7262@gmail.com>
2023-06-03 23:01:10 +0800

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline bg-green-200">
+        Connect Four
+      </h1>
+    </>
+  );
+}
+
+export default App;
