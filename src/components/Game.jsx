9262777d05fe82b01a8fe2f0c7f23bd409f0ea86
Jedidiah Carl O. Tan <masterjed7262@gmail.com>
2023-06-04 17:04:48 +0800

    style: add game menu

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -1,14 +1,17 @@
+import Logo from "../assets/images/logo.svg";
 import PlayerOne from "../assets/images/player-one.svg";
 import PlayerTwo from "../assets/images/player-two.svg";
+import LargeWhiteBoard from "../assets/images/board-layer-white-large.svg";
+import LargeBlackBoard from "../assets/images/board-layer-black-large.svg";
 
 const Game = () => {
   return (
-    <div className="flex gap-x-[60px] items-center">
+    <div className="bg-purple w-screen flex gap-x-[60px] items-center justify-center">
       <div className="rounded-[20px] bg-white w-[141px] h-[160px] border-[3px] border-black shadow-thick-black">
         <div className="flex flex-col items-center relative">
           <img
             className="w-[54px] h-[59px] absolute -top-[27px] 
-            "
+                "
             src={PlayerOne}
             alt=""
           />
@@ -18,12 +21,26 @@ const Game = () => {
           <div className="text-lg font-spacegrotesk font-bold">0</div>
         </div>
       </div>
-      <div>Board</div>
+      <div className="flex flex-col">
+        <div className="flex w-[632px] justify-between mb-[51px]">
+          <div className="font-bold flex items-center justify-center rounded-[20px] bg-iris text-xs font-spacegrotesk text-white w-[86px] h-[39px]">
+            <div>MENU</div>
+          </div>
+          <img src={Logo} alt="" />
+          <div className=" font-bold flex items-center justify-center rounded-[20px] bg-iris text-xs font-spacegrotesk text-white w-[108px] h-[39px]">
+            <div>RESTART</div>
+          </div>
+        </div>
+        <div>
+          <img className="absolute" src={LargeWhiteBoard} alt="" />
+          <img src={LargeBlackBoard} alt="" />
+        </div>
+      </div>
       <div className="rounded-[20px] bg-white w-[141px] h-[160px] border-[3px] border-black shadow-thick-black">
         <div className="flex flex-col items-center relative">
           <img
             className="w-[54px] h-[59px] absolute -top-[27px] 
-            "
+                "
             src={PlayerTwo}
             alt=""
           />
