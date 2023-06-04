351cb6b6d2185b95c22dc0ab1a5f05654be02ee5
Jedidiah Carl O. Tan <masterjed7262@gmail.com>
2023-06-04 18:34:21 +0800

    feat: implement pause menu

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -5,69 +5,122 @@ import LargeWhiteBoard from "../assets/images/board-layer-white-large.svg";
 import LargeBlackBoard from "../assets/images/board-layer-black-large.svg";
 import MarkerRed from "../assets/images/marker-red.svg";
 import TurnBackgroundRed from "../assets/images/turn-background-red.svg";
+import { useState } from "react";
+import { Link } from "react-router-dom";
+// import Popup from "reactjs-popup";
+// const PopupExample = () => (
+//   <Popup trigger={<button> Trigger</button>} position="right center">
+//     <div>Popup content here !!</div>
+//   </Popup>
+// );
 
 const Game = () => {
+  const [isPaused, togglePause] = useState(false);
+
+  const menuClick = () => {
+    console.log("Test");
+    togglePause(!isPaused);
+  };
+
   return (
-    <div className="bg-purple w-screen flex gap-x-[60px] items-center justify-center relative">
-      <div className=" rounded-[20px] bg-white w-[141px] h-[160px] border-[3px] border-black shadow-thick-black">
-        <div className=" flex flex-col items-center relative">
-          <img
-            className=" w-[54px] h-[59px] absolute -top-[27px] 
+    <>
+      <div
+        className={`${
+          isPaused ? "visible" : "invisible"
+        } opacity-100 self-center z-50 rounded-[40px] w-[480px] h-[491px] bg-purple border-[3px] border-black absolute top-0 mt-[230px] shadow-thick-black flex flex-col items-center`}
+      >
+        <div className="text-lg font-spacegrotesk font-bold text-white mt-[50px] mb-[44px]">
+          PAUSE
+        </div>
+        <button
+          onClick={menuClick}
+          className="flex justify-between bg-white rounded-[20px] p-5 w-[400px] border-[3px] border-black shadow-thick-black mb-[27px] hover:border-iris hover:shadow-thick-purple"
+        >
+          <div className="text-md font-spacegrotesk font-bold hover:border-iris  ">
+            CONTINUE GAME
+          </div>
+        </button>
+        <button className="flex justify-between bg-white rounded-[20px] p-5 w-[400px] border-[3px] border-black shadow-thick-black mb-[27px] hover:border-iris hover:shadow-thick-purple">
+          <div className="text-md font-spacegrotesk font-bold hover:border-iris  ">
+            RESTART
+          </div>
+        </button>
+        <Link to="/">
+          <div className="flex justify-between bg-coral-pink rounded-[20px] p-5 w-[400px] border-[3px] border-black shadow-thick-black mb-[27px] hover:border-iris hover:shadow-thick-purple">
+            <div className="text-md font-spacegrotesk font-bold hover:border-iris  ">
+              QUIT GAME
+            </div>
+          </div>
+        </Link>
+      </div>
+      <div
+        className={`${
+          isPaused ? "opacity-50" : ""
+        } bg-purple w-screen flex gap-x-[60px] items-center justify-center relative`}
+      >
+        <div className=" rounded-[20px] bg-white w-[141px] h-[160px] border-[3px] border-black shadow-thick-black">
+          <div className=" flex flex-col items-center relative">
+            <img
+              className=" w-[54px] h-[59px] absolute -top-[27px] 
                 "
-            src={PlayerOne}
-            alt=""
-          />
-          <div className="text-sm font-spacegrotesk font-bold mt-[46px]">
-            PLAYER 1
-          </div>
-          <div className="text-lg font-spacegrotesk font-bold">0</div>
-        </div>
-      </div>
-      <div className="z-50 flex flex-col relative">
-        <div className="flex w-[632px] justify-between mb-[51px]">
-          <div className="font-bold flex items-center justify-center rounded-[20px] bg-iris text-xs font-spacegrotesk text-white w-[86px] h-[39px] hover:bg-coral-pink">
-            <div>MENU</div>
-          </div>
-          <img src={Logo} alt="" />
-          <div className=" font-bold flex items-center justify-center rounded-[20px] bg-iris text-xs font-spacegrotesk text-white w-[108px] h-[39px] hover:bg-coral-pink">
-            <div>RESTART</div>
+              src={PlayerOne}
+              alt=""
+            />
+            <div className="text-sm font-spacegrotesk font-bold mt-[46px]">
+              PLAYER 1
+            </div>
+            <div className="text-lg font-spacegrotesk font-bold">0</div>
           </div>
         </div>
-        <img className="ml-[38px] w-[32px] h-[36px]" src={MarkerRed} alt="" />
-        <div>
-          <img className="absolute" src={LargeWhiteBoard} alt="" />
-          <img src={LargeBlackBoard} alt="" />
-        </div>
-        <div className="flex flex-col items-center justify-center relative  -top-[16px]">
-          <div className="z-50 text-white font-spacegrotesk text-xs font-bold">
-            PLAYER 1&apos;S TURN
+        <div className="z-40 flex flex-col relative">
+          <div className="flex w-[632px] justify-between mb-[12px] relative">
+            <button
+              onClick={menuClick}
+              className="font-bold flex items-center justify-center rounded-[20px] bg-iris text-xs font-spacegrotesk text-white w-[86px] h-[39px] hover:bg-coral-pink"
+            >
+              <div>MENU</div>
+            </button>
+            <img src={Logo} alt="" />
+            <div className=" font-bold flex items-center justify-center rounded-[20px] bg-iris text-xs font-spacegrotesk text-white w-[108px] h-[39px] hover:bg-coral-pink">
+              <div>RESTART</div>
+            </div>
           </div>
-          <div className="z-50 text-white font-spacegrotesk text-lg font-bold">
-            30s
+          <img className="ml-[38px] w-[32px] h-[36px]" src={MarkerRed} alt="" />
+          <div className="relative flex justify-center">
+            <img className="absolute" src={LargeWhiteBoard} alt="" />
+            <img src={LargeBlackBoard} alt="" />
           </div>
-          <img
-            className="w-[191px] h-[150px] absolute -top-[37px]"
-            src={TurnBackgroundRed}
-            alt=""
-          />
+          <div className="flex flex-col items-center justify-center relative  -top-[16px]">
+            <div className="z-50 text-white font-spacegrotesk text-xs font-bold">
+              PLAYER 1&apos;S TURN
+            </div>
+            <div className="z-50 text-white font-spacegrotesk text-lg font-bold">
+              30s
+            </div>
+            <img
+              className="w-[191px] h-[150px] absolute -top-[37px]"
+              src={TurnBackgroundRed}
+              alt=""
+            />
+          </div>
         </div>
-      </div>
-      <div className="rounded-[20px] bg-white w-[141px] h-[160px] border-[3px] border-black shadow-thick-black">
-        <div className="flex flex-col items-center relative">
-          <img
-            className="w-[54px] h-[59px] absolute -top-[27px] 
+        <div className="rounded-[20px] bg-white w-[141px] h-[160px] border-[3px] border-black shadow-thick-black">
+          <div className="flex flex-col items-center relative">
+            <img
+              className="w-[54px] h-[59px] absolute -top-[27px] 
                 "
-            src={PlayerTwo}
-            alt=""
-          />
-          <div className="text-sm font-spacegrotesk font-bold mt-[46px]">
-            PLAYER 2
+              src={PlayerTwo}
+              alt=""
+            />
+            <div className="text-sm font-spacegrotesk font-bold mt-[46px]">
+              PLAYER 2
+            </div>
+            <div className="text-lg font-spacegrotesk font-bold">0</div>
           </div>
-          <div className="text-lg font-spacegrotesk font-bold">0</div>
         </div>
+        <div className="z-0 rounded-t-[60px] h-[200px] w-screen bg-iris absolute bottom-0"></div>
       </div>
-      <div className="z-0 rounded-t-[60px] h-[200px] w-screen bg-iris absolute bottom-0"></div>
-    </div>
+    </>
   );
 };
 export default Game;
