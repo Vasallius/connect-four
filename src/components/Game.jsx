770d33f95d36534c27e329b59705b588cc6acd5f
Jedidiah Carl O. Tan <masterjed7262@gmail.com>
2023-06-04 17:30:31 +0800

    style: add turn marker

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -4,6 +4,7 @@ import PlayerTwo from "../assets/images/player-two.svg";
 import LargeWhiteBoard from "../assets/images/board-layer-white-large.svg";
 import LargeBlackBoard from "../assets/images/board-layer-black-large.svg";
 import MarkerRed from "../assets/images/marker-red.svg";
+import TurnBackgroundRed from "../assets/images/turn-background-red.svg";
 
 const Game = () => {
   return (
@@ -22,7 +23,7 @@ const Game = () => {
           <div className="text-lg font-spacegrotesk font-bold">0</div>
         </div>
       </div>
-      <div className="flex flex-col">
+      <div className="flex flex-col relative">
         <div className="flex w-[632px] justify-between mb-[51px]">
           <div className="font-bold flex items-center justify-center rounded-[20px] bg-iris text-xs font-spacegrotesk text-white w-[86px] h-[39px]">
             <div>MENU</div>
@@ -36,6 +37,19 @@ const Game = () => {
         <div>
           <img className="absolute" src={LargeWhiteBoard} alt="" />
           <img src={LargeBlackBoard} alt="" />
+        </div>
+        <div className="flex flex-col items-center justify-center relative  -top-[16px]">
+          <div className="z-50 text-white font-spacegrotesk text-xs font-bold">
+            PLAYER 1&apos;S TURN
+          </div>
+          <div className="z-50 text-white font-spacegrotesk text-lg font-bold">
+            30s
+          </div>
+          <img
+            className="w-[191px] h-[150px] absolute -top-[37px]"
+            src={TurnBackgroundRed}
+            alt=""
+          />
         </div>
       </div>
       <div className="rounded-[20px] bg-white w-[141px] h-[160px] border-[3px] border-black shadow-thick-black">
