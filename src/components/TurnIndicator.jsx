1249f6e050a0b1498fc1fb87466de9d35fc9cab4
Jedidiah Carl O. Tan <masterjed7262@gmail.com>
2023-06-09 12:07:17 +0800

    feat: add count down

--- src/components/TurnIndicator.jsx
+++ src/components/TurnIndicator.jsx
@@ -1,10 +1,32 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 /* eslint-disable react/prop-types */
 import TurnBackgroundRed from "../assets/images/turn-background-red.svg";
 import TurnBackgroundYellow from "../assets/images/turn-background-yellow.svg";
 import useStore from "../stores/useStore";
+import { useEffect } from "react";
 
 export function TurnIndicator() {
   const turn = useStore((state) => state.turn);
+  const setTurn = useStore((state) => state.setTurn);
+  const counter = useStore((state) => state.counter);
+  const setCounter = useStore((state) => state.setCounter);
+  useEffect(() => {
+    if (counter > 0) {
+      const interval = setInterval(() => {
+        setCounter(counter - 1);
+        console.log(counter);
+      }, 1000);
+
+      return () => clearInterval(interval); // Cleanup the interval when the component unmounts
+    } else {
+      setCounter(30);
+      if (turn === "red") {
+        setTurn("yellow");
+      } else {
+        setTurn("red");
+      }
+    }
+  }, [counter, setCounter]);
 
   return (
     <div className="z-50 flex flex-col items-center justify-center relative  -top-[16px]">
@@ -20,7 +42,7 @@ export function TurnIndicator() {
           turn === "red" ? "text-white" : "text-black"
         } font-spacegrotesk text-lg font-bold`}
       >
-        30s
+        {counter}s
       </div>
       <img
         className="w-[191px] h-[150px] absolute -top-[37px]"
--- src/stores/useStore.js
+++ src/stores/useStore.js
@@ -4,9 +4,11 @@ const useStore = create((set) => ({
   turn: "red",
   markers: [],
   board: [[], [], [], [], [], [], []],
+  counter: 30,
   setTurn: (turn) => set(() => ({ turn: turn })),
   setMarkers: (markers) => set(() => ({ markers: markers })),
   setBoard: (board) => set(() => ({ board: board })),
+  setCounter: (counter) => set(() => ({ counter: counter })),
 }));
 
 export default useStore;
