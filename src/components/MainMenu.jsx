efdd3c54cacf8013bb884a42880fe696210d4848
Jedidiah Carl O. Tan <masterjed7262@gmail.com>
2023-06-13 10:36:34 +0800

    refactor: improve accessibility
    - use semantic html
    - added alt on img

--- src/components/MainMenu.jsx
+++ src/components/MainMenu.jsx
@@ -4,25 +4,29 @@ import { Link } from "react-router-dom";
 
 const MainMenu = () => {
   return (
-    <div className="bg-purple h-[435px] w-[480px] flex flex-col items-center mt-[120px] justify-center gap-y-2 rounded-[40px] border-[3px] border-black shadow-thick-black">
-      <img className="mt-[70px] mb-[79px]" src={Logo} alt="" />
+    <main className="bg-purple h-[435px] w-[480px] flex flex-col items-center mt-[120px] justify-center gap-y-2 rounded-[40px] border-[3px] border-black shadow-thick-black">
+      <img className="mt-[70px] mb-[79px]" src={Logo} alt="Connect Four Logo" />
       <Link to="/game">
-        <div className="flex items-center justify-between bg-dandelion rounded-[20px] p-5 w-[400px] border-[3px] border-black shadow-thick-black mb-[30px] hover:border-iris hover:shadow-thick-purple">
-          <div className="text-md font-spacegrotesk font-bold ">
+        <button className="flex items-center justify-between bg-dandelion rounded-[20px] p-5 w-[400px] border-[3px] border-black shadow-thick-black mb-[30px] hover:border-iris hover:shadow-thick-purple">
+          <h1 className="text-md font-spacegrotesk font-bold ">
             PLAY VS PLAYER
-          </div>
-          <img className="" src={PlayervsPlayer} alt="" />
-        </div>
+          </h1>
+          <img
+            className=""
+            src={PlayervsPlayer}
+            alt="Play versus Player Icon"
+          />
+        </button>
       </Link>
 
       <Link to="/rules">
-        <div className="flex justify-between bg-white rounded-[20px] p-5 w-[400px] border-[3px] border-black shadow-thick-black mb-[60px] hover:border-iris hover:shadow-thick-purple">
-          <div className="text-md font-spacegrotesk font-bold hover:border-iris  ">
+        <button className="flex justify-between bg-white rounded-[20px] p-5 w-[400px] border-[3px] border-black shadow-thick-black mb-[60px] hover:border-iris hover:shadow-thick-purple">
+          <h1 className="text-md font-spacegrotesk font-bold hover:border-iris  ">
             GAME RULES
-          </div>
-        </div>
+          </h1>
+        </button>
       </Link>
-    </div>
+    </main>
   );
 };
 export default MainMenu;
