19ee38a615d0324e5104cd5962930f2b622a6632
Jedidiah Carl O. Tan <masterjed7262@gmail.com>
2023-06-13 10:56:34 +0800

    refactor: improve axe

--- src/components/Rules.jsx
+++ src/components/Rules.jsx
@@ -3,57 +3,55 @@ import IconCheck from "../assets/images/icon-check.svg";
 
 const Rules = () => {
   return (
-    <div className="bg-white h-[537px] w-[480px] flex flex-col items-center mt-[182px]  gap-y-2 rounded-[40px] border-[3px] border-black shadow-thick-black">
-      <div className="text-lg font-bold mt-[30px] mb-[29px]">RULES</div>
-      <div className="flex flex-col px-[34px] ">
-        <div className="text-purple text-sm font-bold font-spacegrotesk mb-[16px]">
+    <main className="bg-white h-[537px] w-[480px] flex flex-col items-center mt-[182px]  gap-y-2 rounded-[40px] border-[3px] border-black shadow-thick-black">
+      <h1 className="text-lg font-bold mt-[30px] mb-[29px]">RULES</h1>
+      <section className="flex flex-col px-[34px] ">
+        <h2 className="text-purple text-sm font-bold font-spacegrotesk mb-[16px]">
           Objective
-        </div>
-        <div className="text-xs font-spacegrotesk opacity-[0.66] mb-[29px]">
+        </h2>
+        <p className="text-xs font-spacegrotesk opacity-[0.66] mb-[29px]">
           Be the first player to connect 4 of the same colored discs in a row
           (either vertically, horizontally, or diagonally).
-        </div>
-        <div className="text-purple text-sm font-bold font-spacegrotesk mb-[16px]">
+        </p>
+        <h2 className="text-purple text-sm font-bold font-spacegrotesk mb-[16px]">
           HOW TO PLAY
-        </div>
-        <div className="flex flex-row gap-x-[19px] mb-[10px]">
-          <div className="text-black font-spacegrotesk text-xs font-bold">
-            1
-          </div>
-          <div className="text-xs font-spacegrotesk opacity-[0.66]">
+        </h2>
+        <section className="flex flex-row gap-x-[19px] mb-[10px]">
+          <p className="text-black font-spacegrotesk text-xs font-bold">1</p>
+          <p className="text-xs font-spacegrotesk opacity-[0.66]">
             Red goes first in the first game.
-          </div>
-        </div>
-        <div className="flex flex-row gap-x-[17px] mb-[10px]">
-          <div className="text-black font-spacegrotesk text-xs font-bold">
-            2
-          </div>
+          </p>
+        </section>
+        <section className="flex flex-row gap-x-[17px] mb-[10px]">
+          <p className="text-black font-spacegrotesk text-xs font-bold">2</p>
           <div className="text-xs font-spacegrotesk opacity-[0.66]">
             Players must alternate turns, and only one disc can be dropped in
             each turn.
           </div>
-        </div>
-        <div className="flex flex-row gap-x-[17px] mb-[10px]">
-          <div className="text-black font-spacegrotesk text-xs font-bold">
-            3
-          </div>
-          <div className="text-xs font-spacegrotesk opacity-[0.66]">
+        </section>
+        <section className="flex flex-row gap-x-[17px] mb-[10px]">
+          <p className="text-black font-spacegrotesk text-xs font-bold">3</p>
+          <p className="text-xs font-spacegrotesk opacity-[0.66]">
             The game ends when there is a 4-in-a-row or a stalemate.
-          </div>
-        </div>
-        <div className="flex flex-row gap-x-[16px] ">
-          <div className="text-black font-spacegrotesk text-xs font-bold">
-            4
-          </div>
-          <div className="text-xs font-spacegrotesk opacity-[0.66] mb-[10px]">
+          </p>
+        </section>
+        <section className="flex flex-row gap-x-[16px] ">
+          <p className="text-black font-spacegrotesk text-xs font-bold">4</p>
+          <p className="text-xs font-spacegrotesk opacity-[0.66] mb-[10px]">
             The starter of the previous game goes second on the next game.
-          </div>
-        </div>
+          </p>
+        </section>
         <Link to="/" className="self-center object-bottom ">
-          <img className="w-[64px] h-[64px] " src={IconCheck} alt="" />
+          <button>
+            <img
+              className="w-[64px] h-[64px] "
+              src={IconCheck}
+              alt="Check Icon"
+            />
+          </button>
         </Link>
-      </div>
-    </div>
+      </section>
+    </main>
   );
 };
 export default Rules;
