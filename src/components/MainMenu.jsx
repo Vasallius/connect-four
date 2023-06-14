c011deb1a7771623c1bbe9a67a8d5474d7f8c742
Jedidiah Carl O. Tan <masterjed7262@gmail.com>
2023-06-15 00:28:35 +0800

    feat: made main menu responsive

--- src/components/MainMenu.jsx
+++ src/components/MainMenu.jsx
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 
 const MainMenu = () => {
   return (
-    <main className="bg-purple h-[435px] w-[480px] flex flex-col items-center mt-[120px] justify-center gap-y-2 rounded-[40px] border-[3px] border-black shadow-thick-black">
+    <main className="bg-purple h-[435px] w-[480px] flex flex-col items-center mt-[203px] md:mt-[295px] lg:mt-[233px] justify-center gap-y-2 rounded-[40px] md:border-[3px] md:border-black md:shadow-thick-black">
       <img className="mt-[70px] mb-[79px]" src={Logo} alt="Connect Four Logo" />
       <Link to="/game">
         <button className="flex items-center justify-between bg-dandelion rounded-[20px] p-5 w-[400px] border-[3px] border-black shadow-thick-black mb-[30px] hover:border-iris hover:shadow-thick-purple">
@@ -21,9 +21,7 @@ const MainMenu = () => {
 
       <Link to="/rules">
         <button className="flex justify-between bg-white rounded-[20px] p-5 w-[400px] border-[3px] border-black shadow-thick-black mb-[60px] hover:border-iris hover:shadow-thick-purple">
-          <h1 className="text-md font-spacegrotesk font-bold hover:border-iris  ">
-            GAME RULES
-          </h1>
+          <h1 className="text-md font-spacegrotesk font-bold">GAME RULES</h1>
         </button>
       </Link>
     </main>
